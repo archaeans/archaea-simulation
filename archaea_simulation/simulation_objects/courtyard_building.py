86c8ae706b60340db95aed86199fee632aec9084
oguzhankoral <oguzhankoral@gmail.com>
2023-03-09 10:57:57 +0300

    Create walls and doors manually
    - Need to parametrize more

--- archaea_simulation/simulation_objects/courtyard_building.py
+++ archaea_simulation/simulation_objects/courtyard_building.py
@@ -1,5 +1,6 @@
 from archaea.geometry.point3d import Point3d
 from archaea.geometry.loop import Loop
+from archaea.geometry.line_segment import LineSegment
 from archaea.geometry.face import Face
 from archaea.geometry.vector3d import Vector3d
 from archaea_simulation.simulation_objects.zone import Zone
@@ -9,7 +10,6 @@ from archaea_simulation.simulation_objects.wall_type import WallType
 
 class CourtyardBuilding:
     zones: "list[Zone]"
-
 
     def __init__(self,
                  center: Point3d,
@@ -50,7 +50,7 @@ class CourtyardBuilding:
         y_shift = self.courtyard_width / 2
 
         zones = []
-        self.create_block_zones(x_center_distance_first_room, y_shift, zones)
+        # self.create_block_zones(x_center_distance_first_room, y_shift, zones)
         self.create_block_zones(x_center_distance_first_room, -y_shift - self.room_depth, zones)
         self.zones = zones
 
@@ -66,20 +66,80 @@ class CourtyardBuilding:
         p1 = Point3d(c.x + x_shift, c.y + y_shift + self.room_depth, c.z)
         p2 = Point3d(c.x + x_shift - self.room_width, c.y + y_shift + self.room_depth, c.z)
         p3 = Point3d(c.x + x_shift - self.room_width, c.y + y_shift, c.z)
+
         ground_loop_1 = Loop([p0, p3, p2, p1])
         ground_face_1 = Face(ground_loop_1)
         ground_wall = Wall(ground_loop_1, [], wall_type=WallType.INNER)
         ceiling_wall = Wall(ground_loop_1.move(Vector3d(0, 0, self.room_height)).reverse(), [], wall_type=WallType.OUTER)
 
-        # zone = Zone(ground_face_1, self.room_height, walls=[ground_wall, ceiling_wall], wall_default_thickness=self.room_wall_thickness)
-        zone = Zone(ground_face_1, self.room_height, wall_default_thickness=self.room_wall_thickness)
+        window_wall = self.create_window_wall(p0, p3)
+
+        door_wall = self.create_door_wall(p2, p1)
+
+        side_wall_1_type = WallType.OUTER
+        if is_start and not is_end:
+            side_wall_1_type = WallType.INNER
+
+        side_wall_2_type = WallType.OUTER
+        if is_end and not is_start:
+            side_wall_2_type = WallType.INNER
+
+        if not is_start and not is_end:
+            side_wall_1_type = WallType.INNER
+            side_wall_2_type = WallType.INNER
+
+        side_wall_line_1 = LineSegment(p3, p2)
+        side_wall_loop_1 = side_wall_line_1.extrude(Vector3d(0, 0, self.room_height))
+        side_wall_1 = Wall(side_wall_loop_1.outer_loop, [], wall_type=side_wall_1_type)
+
+        side_wall_line_2 = LineSegment(p1, p0)
+        side_wall_loop_2 = side_wall_line_2.extrude(Vector3d(0, 0, self.room_height))
+        side_wall_2 = Wall(side_wall_loop_2.outer_loop, [], wall_type=side_wall_2_type)
+
+        zone = Zone(
+            ground_face_1,
+            self.room_height,
+            walls=[window_wall, door_wall, side_wall_1, side_wall_2],
+            wall_default_thickness=self.room_wall_thickness
+        )
+        # zone = Zone(ground_face_1, self.room_height, wall_default_thickness=self.room_wall_thickness)
         return zone
 
     def create_window_wall(self, p_start: Point3d, p_end: Point3d):
         window_sill_height = 0.9
-        wall_width = p_start.distance_to(p_end)
+
+        window_wall_line = LineSegment(p_start, p_end)
+
+        moved_wall_line = window_wall_line.move(Vector3d(0, 0, window_sill_height))
+        window_start_point = moved_wall_line.point_at(0.4)
+        window_end_point = moved_wall_line.point_at(0.6)
+        window_line = LineSegment(window_start_point, window_end_point)
+        window_loop = window_line.extrude(Vector3d(0, 0, self.room_window_height))
+
+        wall_width = window_wall_line.length
         width_ratio = (self.room_width - self.room_window_width) / self.room_window_width
-        p_end_top = p_end.move(Vector3d(0, 0, self.room_height))
-        p_start_top = p_start.move(Vector3d(0, 0, self.room_height))
-        wall_outer_loop = Loop([p_start, p_end, p_end_top, p_start_top])
+
+        window_wall_loop = window_wall_line.extrude(Vector3d(0, 0, self.room_height))
+        window_wall = Wall(window_wall_loop.outer_loop, [window_loop.outer_loop], wall_type=WallType.OUTER)
+
+        return window_wall
+
+    def create_door_wall(self, p_start: Point3d, p_end: Point3d):
+        door_sill_height = 0.2
+
+        door_wall_line = LineSegment(p_start, p_end)
+
+        moved_wall_line = door_wall_line.move(Vector3d(0, 0, door_sill_height))
+        door_start_point = moved_wall_line.point_at(0.1)
+        door_end_point = moved_wall_line.point_at(0.32)
+        door_line = LineSegment(door_start_point, door_end_point)
+        door_loop = door_line.extrude(Vector3d(0, 0, self.room_door_height))
+
+        wall_width = door_wall_line.length
+        width_ratio = (self.room_width - self.room_window_width) / self.room_window_width
+
+        door_wall_loop = door_wall_line.extrude(Vector3d(0, 0, self.room_height))
+        door_wall = Wall(door_wall_loop.outer_loop, [door_loop.outer_loop], wall_type=WallType.OUTER)
+
+        return door_wall
 
