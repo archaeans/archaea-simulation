c667426e0c763d631ca3a2d8b9b2c7b553be76cb
oguzhankoral <oguzhankoral@gmail.com>
2023-10-25 00:35:15 +0300

    Reduce context mesh refinement level from 3 to 2

--- archaea_simulation/cfd/utils/snappyHexMeshDict.py
+++ archaea_simulation/cfd/utils/snappyHexMeshDict.py
@@ -12,9 +12,9 @@ def snappy_hex_mesh_features(eMesh_name: str, level: int) -> str:
            '\tlevel {1};\n' \
            '    }}\n'.format(eMesh_name, level)  # noqa: UP030
 
-def snappy_hex_mesh_refinementSurfaces(geometry_name: str) -> str:
+def snappy_hex_mesh_refinementSurfaces(geometry_name: str, level_1: str, level_2: str) -> str:
     return '{0}\n' \
            '    {{\n' \
-           '\tlevel (3 3);\n' \
+           '\tlevel ({1} {2});\n' \
            '\tpatchInfo {{ type wall; }}\n' \
-           '    }}\n'.format(geometry_name)  # noqa: F524, UP030
+           '    }}\n'.format(geometry_name, level_1, level_2)  # noqa: F524, UP030
