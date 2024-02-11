4512dccc607dbbb361e61a1e7b44cb3112f04041
oguzhankoral <oguzhankoral@gmail.com>
2024-02-12 00:24:47 +0300

    Run energy simulations via openstudio

--- archaea_simulation/simulation_objects/thermal_zone.py
+++ archaea_simulation/simulation_objects/thermal_zone.py
@@ -2,6 +2,8 @@ from honeybee.room import Room as HB_Room
 from honeybee.face import Face as HB_Face
 
 from archaea_simulation.bes.schedule.program_types import conditioned_program
+from archaea_simulation.bes.material.construction_set import construction_set
+from honeybee_energy.lib.programtypes import office_program
 
 
 class ThermalZone:
@@ -19,5 +21,6 @@ class ThermalZone:
         self.identifier = identifier
         self.faces = faces
         self.room = HB_Room(identifier, faces)
-        self.room.properties.energy.add_default_ideal_air() if is_conditioned else None
         self.room.properties.energy.program_type = conditioned_program
+        self.room.properties.energy.add_default_ideal_air()
+        self.room.properties.energy.construction_set = construction_set
